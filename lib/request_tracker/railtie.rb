add168e69f98b80701cfe0747fab7ddd1a475b79
Steven Wanderski <steven.wanderski@gmail.com>
2026-06-26 14:21:05 -0500

    Adds Sidekiq middleware initializer

--- lib/request_tracker/railtie.rb
+++ lib/request_tracker/railtie.rb
@@ -10,5 +10,15 @@ module RequestTracker
 
       raise ConfigurationError.new(missing) if missing.any?
     end
+
+    initializer "request_tracker.sidekiq_middleware" do
+      if defined?(Sidekiq)
+        Sidekiq.configure_client do |config|
+          config.client_middleware do |chain|
+            chain.add RequestTracker::SidekiqClientMiddleware
+          end
+        end
+      end
+    end
   end
 end
